0902288b1548ffbbe2702468232dfa2b6e4a29e5
Mark Canlas <github@htmlism.com>
2020-08-26 01:28:36 -0400

    add snake length; more documentation

--- src/main/scala/com/htmlism/mos6502/dsl/ReadWriteLocation.scala
+++ src/main/scala/com/htmlism/mos6502/dsl/ReadWriteLocation.scala
@@ -3,9 +3,11 @@ package com.htmlism.mos6502.dsl
 //import com.htmlism.mos6502.model._
 
 /**
+  * @param name A name for this location, used to alias its address
+  *
   * @tparam A The input type of the write and the output type of the read
   */
-case class ReadWriteLocation[A](address: Address) {
+case class ReadWriteLocation[A](name: String, address: Address) {
   def read(implicit ctx: AssemblyContext): Unit = {
     val _ = ctx
 //    ctx.push(LDA, ev, s"write ${ev.toShow(x)} to $name ($n)")
--- src/main/scala/com/htmlism/mos6502/dsl/VolatileDevice.scala
+++ src/main/scala/com/htmlism/mos6502/dsl/VolatileDevice.scala
@@ -3,9 +3,13 @@ package com.htmlism.mos6502.dsl
 //import com.htmlism.mos6502.model._
 
 /**
+  * For a memory-mapped device that may return different values across multiple reads (e.g. a random number generator)
+  *
+  * @param name A name for this device, used to alias its address
+  *
   * @tparam A The return type of the read
   */
-case class VolatileDevice[A](address: Address) {
+case class VolatileDevice[A](name: String, address: Address) {
   def read(implicit ctx: AssemblyContext): Unit = {
     val _ = ctx
 //    ctx.push(LDA, ev, s"write ${ev.toShow(x)} to $name ($n)")
