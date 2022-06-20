397ba5a92dd95f0041c4c0f922f095a2772fa389
Mark Canlas <github@htmlism.com>
2022-06-20 12:07:16 -0400

    split for sbt gha

--- project/Scala3Plugin.scala
+++ project/Scala3Plugin.scala
@@ -11,8 +11,11 @@ object Scala3Plugin extends AutoPlugin {
     */
   override def trigger: PluginTrigger = AllRequirements
 
+  override val buildSettings: Seq[Setting[_]] = Seq(
+    scalaVersion := "3.1.2"
+  )
+
   override val projectSettings: Seq[Setting[_]] = Seq(
-    scalaVersion := "3.1.2",
     scalacOptions ++= Seq("-indent", "-rewrite")
   )
 }
