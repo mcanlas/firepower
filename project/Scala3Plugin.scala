92aef1c71cf5d6c71520dd9d7f1cdbcfcfe31072
Mark Canlas <github@htmlism.com>
2022-06-23 06:44:34 -0400

    scala 3.1.3

--- project/Scala3Plugin.scala
+++ project/Scala3Plugin.scala
@@ -12,7 +12,7 @@ object Scala3Plugin extends AutoPlugin {
   override def trigger: PluginTrigger = AllRequirements
 
   override val buildSettings: Seq[Setting[_]] = Seq(
-    scalaVersion := "3.1.2"
+    scalaVersion := "3.1.3"
   )
 
   override val projectSettings: Seq[Setting[_]] = Seq(
