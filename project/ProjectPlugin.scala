1aa41872c2c121ce7770231d0f7882c35ef38c19
Mark Canlas <github@htmlism.com>
2022-11-14 15:49:53 -0500

    cats-core 2.9.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -9,7 +9,7 @@ object ProjectPlugin extends AutoPlugin {
   object ThingsToImport {
     implicit class ProjectOps(p: Project) {
       def withCats: Project =
-        p.settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.8.0")
+        p.settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.9.0")
 
       def withTesting: Project =
         p.settings(libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.14" % "test")
