6582f866fb44c9f1a679db961333aa1ca02a09a2
Mark Canlas <github@htmlism.com>
2022-11-19 21:42:48 -0500

    include weaver

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -11,8 +11,19 @@ object ProjectPlugin extends AutoPlugin {
       def withCats: Project =
         p.settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.9.0")
 
-      def withTesting: Project =
-        p.settings(libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.14" % "test")
+      def withTesting: Project = {
+        val weaverVersion =
+          "0.8.0"
+
+        p.settings(
+          testFrameworks += new TestFramework("weaver.framework.CatsEffect"),
+          libraryDependencies ++= Seq(
+            "org.scalatest"       %% "scalatest"         % "3.2.14"      % Test,
+            "com.disneystreaming" %% "weaver-cats"       % weaverVersion % Test,
+            "com.disneystreaming" %% "weaver-scalacheck" % weaverVersion % Test
+          )
+        )
+      }
     }
   }
 }
