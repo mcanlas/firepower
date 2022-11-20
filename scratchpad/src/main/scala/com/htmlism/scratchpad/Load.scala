3fc89ae23d1cb6b0e31d7d157343adac19e7dbbb
Mark Canlas <github@htmlism.com>
2022-11-19 23:11:10 -0500

    use standard name

--- scratchpad/src/main/scala/com/htmlism/scratchpad/Load.scala
+++ scratchpad/src/main/scala/com/htmlism/scratchpad/Load.scala
@@ -1,9 +1,9 @@
 package com.htmlism.scratchpad
 
 object Load:
-  def const[R: Register, X: Encoded.Byte](x: X): Asm1[R] =
+  def const[R: Register, A: Encoded.Byte](x: A): Asm1[R] =
     Const(x)
 
-  case class Const[R, X: Encoded.Byte](x: X)(using R: Register[R]) extends Asm1[R]:
+  case class Const[R, A: Encoded.Byte](x: A)(using R: Register[R]) extends Asm1[R]:
     def xs: List[String] =
       List("LD" + R.name)
