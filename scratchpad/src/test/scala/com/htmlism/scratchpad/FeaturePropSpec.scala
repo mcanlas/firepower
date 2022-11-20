202a1eeba4af2c1122da82e55b489e99d661cb2f
Mark Canlas <github@htmlism.com>
2022-11-19 21:55:31 -0500

    create weaver stub

--- scratchpad/src/test/scala/com/htmlism/scratchpad/FeaturePropSpec.scala
+++
@@ -1,6 +0,0 @@
-package com.htmlism.scratchpad
-
-import org.scalatest.propspec._
-import org.scalatest.matchers.should._
-
-class FeaturePropSpec extends AnyPropSpec with Matchers
---
+++ scratchpad/src/test/scala/com/htmlism/scratchpad/MoveSuite.scala
@@ -0,0 +1,16 @@
+package com.htmlism.scratchpad
+
+import weaver._
+
+object MoveSuite extends FunSuite:
+  test("move constant") {
+    success
+  }
+
+  test("move register") {
+    success
+  }
+
+  test("move register, indexed") {
+    success
+  }
