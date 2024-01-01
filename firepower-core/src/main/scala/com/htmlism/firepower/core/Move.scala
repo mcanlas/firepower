ee87a131ead602e5013ee3ac63619ad93826a21c
Mark Canlas <github@htmlism.com>
2023-12-31 19:02:40 -0500

    avoid deprecated syntax

--- firepower-core/src/main/scala/com/htmlism/firepower/core/Move.scala
+++ firepower-core/src/main/scala/com/htmlism/firepower/core/Move.scala
@@ -5,7 +5,8 @@ object Move:
   def constA[A, X: Encoded.Byte](x: X, dest: WriteLease.ByteAddress[A]): Asm2[Reg.A, A] =
     Load
       .constA(x)
-      .widenWith[A] andThen Store.fromA(dest)
+      .widenWith[A]
+      .andThen(Store.fromA(dest))
 
   def from[R: Register, A: ReadLease.ByteAddress, B: WriteLease.ByteAddress]: Asm3[R, A, B] =
     Asm3Instructions(List(""))
