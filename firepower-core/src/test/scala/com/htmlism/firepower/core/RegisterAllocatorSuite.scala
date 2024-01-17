a3d85136123e91df016ce54e52ec21be39bb3705
Mark Canlas <github@htmlism.com>
2024-01-16 20:10:31 -0500

    outline specs

--- firepower-core/src/test/scala/com/htmlism/firepower/core/RegisterAllocatorSuite.scala
+++ firepower-core/src/test/scala/com/htmlism/firepower/core/RegisterAllocatorSuite.scala
@@ -3,6 +3,14 @@ package com.htmlism.firepower.core
 import weaver.FunSuite
 
 object RegisterAllocatorSuite extends FunSuite:
-  // TODO
   test("cam allocate registers"):
-    expect.eql(1, 1)
+    expect.eql("TODO", "TODO")
+
+  test("A register can be allocated to a single byte width"):
+    expect.eql("TODO", "TODO")
+
+  test("Allocates CPU registers first, then zero page, then global"):
+    expect.eql("TODO", "TODO")
+
+  test("A register can be allocated to a two-byte width, always a memory register"):
+    expect.eql("TODO", "TODO")
